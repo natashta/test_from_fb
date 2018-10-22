c695c2723fb2cde50764bdedeb8a2012d64439ba
Natalia Ryzhova <natashta@gmail.com>
2018-10-22 21:23:43 +0300

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,41 @@
+'use strict'
+
+var slides = ['https://raw.githubusercontent.com/natashta/testfb/master/img/%D1%88%D0%B0%D0%BF%D0%BA%D0%B0.jpg',
+ 'https://raw.githubusercontent.com/natashta/testfb/master/img/%D1%88%D0%B0%D0%BF%D0%BA%D0%B02.jpg',
+  'https://raw.githubusercontent.com/natashta/testfb/master/img/%D1%88%D0%B0%D0%BF%D0%BA%D0%B03.jpg'];
+var slidesProcess = ['https://raw.githubusercontent.com/natashta/testfb/master/img/%D0%BF%D1%80%D0%BE%D1%86%D0%B5%D1%81%D1%81.jpg',
+ 'https://raw.githubusercontent.com/natashta/testfb/master/img/%D0%BF%D1%80%D0%BE%D1%86%D0%B5%D1%81%D1%812.jpg',
+ 'https://raw.githubusercontent.com/natashta/testfb/master/img/%D0%BF%D1%80%D0%BE%D1%86%D0%B5%D1%81%D1%81.jpg'];
+//let slider = document.getElementsByTagName('header');
+let slider = document.getElementById('slider');
+let sliderProc = document.querySelector('.process');
+let dots = document.querySelectorAll('.dot');
+
+let index = 0;
+
+function showImg() {
+  slider.style.backgroundImage = "url("+ slides[index] +")"; 
+  for(var i = 0 ; i < slides.length ; i++ ){
+        dots[i].classList.remove('active-dot');
+    } 
+  dots[index].classList.add('active-dot');
+  index++;
+  if (index === slides.length) {
+    index = 0;
+  }
+}
+setInterval(showImg, 5000);
+
+function showImgProc() {
+  sliderProc.style.backgroundImage = "url("+ slidesProcess[index] +")"; 
+  for(var i = 0 ; i < slidesProcess.length ; i++ ){
+        dots[i].classList.remove('active-dot');
+    } 
+  dots[index].classList.add('active-dot');
+  index++;
+  if (index === slidesProcess.length) {
+    index = 0;
+  }
+}
+setInterval(showImgProc, 5000);
+
