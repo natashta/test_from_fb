b8d4603627dac07b8fdd63e459ee9baf614dd94e
Natalia Ryzhova <natashta@gmail.com>
2018-12-03 12:26:03 +0300

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,11 +1,11 @@
 'use strict'
 
-var slides = ['img/шапка.jpg',
- 'img/шапка2.jpg',
-  'img/шапка3.jpg'];
-var slidesProcess = ['img/процесс.jpg',
- 'img/процесс1.jpg',
- 'img/процесс.jpg'];
+var slides = ['https://github.com/natashta/testfb/blob/master/img/%D1%88%D0%B0%D0%BF%D0%BA%D0%B0.jpg?raw=true',
+ 'https://github.com/natashta/testfb/blob/master/img/%D1%88%D0%B0%D0%BF%D0%BA%D0%B02.jpg?raw=true',
+  'https://github.com/natashta/testfb/blob/master/img/%D1%88%D0%B0%D0%BF%D0%BA%D0%B03.jpg?raw=true'];
+var slidesProcess = ['https://github.com/natashta/testfb/blob/master/img/%D0%BF%D1%80%D0%BE%D1%86%D0%B5%D1%81%D1%81.jpg?raw=true',
+ 'https://github.com/natashta/testfb/blob/master/img/%D0%BF%D1%80%D0%BE%D1%86%D0%B5%D1%81%D1%812.jpg?raw=true',
+ 'https://github.com/natashta/testfb/blob/master/img/%D0%BF%D1%80%D0%BE%D1%86%D0%B5%D1%81%D1%81.jpg?raw=true'];
 //let slider = document.getElementsByTagName('header');
 let slider = document.getElementById('slider');
 let sliderProc = document.querySelector('.process');
